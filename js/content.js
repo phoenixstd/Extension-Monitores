16331bf74f0a4fcd7eb875707c054e40c99bc695
Sebastian Garzon <sebastiangarzon2001@gmail.com>
2023-04-19 20:54:09 -0500

    Agregamos script de reconocimiento de estado de la transmision de la modelo que este transmitiendo desde el mismo computador

--- js/content.js
+++ js/content.js
@@ -3,6 +3,7 @@
 ----------------------------------------*/
 let dominio = "apps.phoenixstd.com";
 let url_api = "https://" + dominio + "/api/";
+//let url_api = "http://localhost/proyectos/" + dominio + "/api/";
 let mensajeText = [];
 let tipperPut = [];
 
@@ -34,73 +35,96 @@ async function putTipperSendMessage(modelo, tipper) {
     }
 }
 
+async function postTransmision(modelo, estado, plataforma) {
+    try {
+        var formData = new FormData();
+        formData.append('modelo', modelo);
+        formData.append('estado', estado);
+        formData.append('plataforma', plataforma);
+        
+        let res = await fetch(url_api+"transmision.php", {
+            method: "POST",
+            body: formData
+        })
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 window.addEventListener('load', (event) => {
     try {
         let perfil = document.querySelector(".user_information_header_username").textContent.toLowerCase();
         //let transmision = document.querySelector(".gender-tab.tabElement.active.tabBorder.activeRoom.tabElementLink").textContent.toLowerCase();
         let url_transmision = window.location.toString();
         if( url_transmision.includes(perfil) && url_transmision.includes("chaturbate.com/b/") ){
-            // Selecciona el elemento que contiene los resultados de la consulta
-            const targetNode = document;
-            // Opciones para el observador (que mutaciones observar)
-            const config = { attributes: false, childList: true, subtree: true };
+            try {
+            
+                // Selecciona el elemento que contiene los resultados de la consulta
+                const targetNode = document;
+                // Opciones para el observador (que mutaciones observar)
+                const config = { attributes: false, childList: true, subtree: true };
 
-            // Crear una instancia del observador con una función callback
-            const observer = new MutationObserver(function(mutationsList, observer) {
-                // Iterar por cada mutación
-                let encontrado = false;
-                for(let mutation of mutationsList) {
-                    // Si se agregó un nuevo nodo
-                    if (mutation.type === 'childList' && mutation.addedNodes.length > 0 && encontrado == false) {
-                        // Verificar si el nuevo nodo contiene resultados de la consulta
-                        const newNodes = mutation.addedNodes;
-                        for (let i = 0; i < newNodes.length; i++) {
-                            try {
-                                const queryResult = newNodes[i].querySelectorAll(".hasBg.isTip.roomNotice > div > div");
-                                if (queryResult.length > 0) {
-                                    var tipper = queryResult[0].textContent;
-                                    console.log(tipper);
-                                    encontrado = true;
-                                    sendCommand("EnviarTipper", tipper, perfil, "");
-                                }
-                            } catch (e) { }
+                // Crear una instancia del observador con una función callback
+                const observer = new MutationObserver(function(mutationsList, observer) {
+                    // Iterar por cada mutación
+                    let encontrado = false;
+                    for(let mutation of mutationsList) {
+                        // Si se agregó un nuevo nodo
+                        if (mutation.type === 'childList' && mutation.addedNodes.length > 0 && encontrado == false) {
+                            // Verificar si el nuevo nodo contiene resultados de la consulta
+                            const newNodes = mutation.addedNodes;
+                            for (let i = 0; i < newNodes.length; i++) {
+                                try {
+                                    const queryResult = newNodes[i].querySelectorAll(".hasBg.isTip.roomNotice > div > div");
+                                    if (queryResult.length > 0) {
+                                        var tipper = queryResult[0].textContent;
+                                        console.log(tipper);
+                                        encontrado = true;
+                                        sendCommand("EnviarTipper", tipper, perfil, "");
+                                    }
+                                } catch (e) { }
+                            }
                         }
                     }
-                }
-            });
+                });
 
-            // Comenzar a observar el nodo objetivo para mutaciones especificadas en la configuración
-            observer.observe(targetNode, config);
+                // Comenzar a observar el nodo objetivo para mutaciones especificadas en la configuración
+                observer.observe(targetNode, config);
+            } catch (error) { }
+            try {
+                // Selecciona el elemento que deseas observar
+                const elementoObservado = document.querySelector("span.roomStatus");
+                var estadoTransmision = "";
+                // Crea una instancia de MutationObserver con una función de devolución de llamada
+                const observador = new MutationObserver(function(mutations) {
+                    mutations.forEach(async function(mutation) {
+                        if( mutation.addedNodes.length > 0 ){
+                            if( estadoTransmision !== mutation.addedNodes[0].data ){
+                                estadoTransmision = mutation.addedNodes[0].data;
+                                //alert(estadoTransmision);
+                                console.log('Estado de Transmision:'+ estadoTransmision);
+                                var textTransmision = await postTransmision(perfil, estadoTransmision, "chaturbate");
+                                if( textTransmision["itemCount"] > 0 ){
+                                    console.log('Estado de Transmision POST ->'+ estadoTransmision);
+                                }
+                            } 
+                        }
+                    });
+                });
+
+                // Configura las opciones para el observador (en este caso, observar cambios en el contenido y atributos)
+                const opcionesObservador = {
+                    childList: true,
+                    attributes: true,
+                    subtree: true
+                };
+
+                // Inicia la observación del elemento observado con las opciones especificadas
+                observador.observe(elementoObservado, opcionesObservador);
+            } catch (error) { }  
         }
-    } catch (error) { }
-    try {
-        // Selecciona el elemento que deseas observar
-        const elementoObservado = document.querySelector("span.roomStatus");
-        var estado = "";
-        // Crea una instancia de MutationObserver con una función de devolución de llamada
-        const observador = new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutation) {
-                if( mutation.addedNodes.length > 0 ){
-                    if( estado !== mutation.addedNodes[0].data ){
-                        var estadoTransmision = mutation.addedNodes[0].data;
-                        alert(estadoTransmision);
-                        console.log('Estado de Transmision:', estadoTransmision);
-                        estado = estadoTransmision;
-                    } 
-                }
-            });
-        });
-
-        // Configura las opciones para el observador (en este caso, observar cambios en el contenido y atributos)
-        const opcionesObservador = {
-            childList: true,
-            attributes: true,
-            subtree: true
-        };
-
-        // Inicia la observación del elemento observado con las opciones especificadas
-        observador.observe(elementoObservado, opcionesObservador);
-    } catch (error) { }  
+    } catch (error) {}
 });
 
 function mensaje(userName, nomTipper) {
@@ -199,8 +223,6 @@ async function gotMessage(request, sender, sendResponse) {
         }
     }
 }
-
-
 
 function sendCommand(command, param, param2, param3) {
     let msg = {
